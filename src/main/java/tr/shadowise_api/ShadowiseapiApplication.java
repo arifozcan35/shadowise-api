53e3770441ab0ab560a2ee6994cb7f09ae0c0155
Arif Özcan <zcanarif@gmail.com>
2025-07-26 23:26:02 +0300

    Initializr

---
+++ src/main/java/tr/shadowise_api/ShadowiseapiApplication.java
@@ -0,0 +1,13 @@
+package tr.shadowise_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowiseapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowiseapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/shadowise_api/ShadowiseapiApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.shadowise_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowiseapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
