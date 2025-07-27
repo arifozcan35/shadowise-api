67952ed412cdd22083fdb1a0da00f8fae4feee13
DANYAL CAN KAYRAK <dancankan@gmail.com>
2025-07-27 12:01:59 +0300

    refactor: models and attributes refactor

--- src/main/java/tr/shadowise_api/entity/Project.java
+++ src/main/java/tr/shadowise_api/entity/Project.java
@@ -2,14 +2,18 @@ package tr.shadowise_api.entity;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "projects")
 public class Project extends SoftDeleteEntity {
     @NotNull
-    private String name;
+    private String title;
     @NotNull
     private String description;
     @NotNull
@@ -18,4 +22,7 @@ public class Project extends SoftDeleteEntity {
     private String[] documents;
     @NotNull
     private String[] insights;
+    private User owner;
+    private List<UploadedFile> uploadedFiles;
+    private List<Note> notes;
 }
